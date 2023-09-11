dbb01eebd90a8f304d863304e5b2aa580c78ed40
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-09-11 16:17:15 -0600

    Fill in simple CRD values

--- api/compute/v1alpha1/virtualmachine_types.go
+++ api/compute/v1alpha1/virtualmachine_types.go
@@ -20,22 +20,18 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
-// VirtualMachineSpec defines the desired state of VirtualMachine
-type VirtualMachineSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of VirtualMachine. Edit virtualmachine_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+type ProviderData struct {
+	Name        string `json:"name,omitempty"`
+	Zone        string `json:"location,omitempty"`
+	MachineType string `json:"machineType,omitempty"`
 }
 
-// VirtualMachineStatus defines the observed state of VirtualMachine
+type VirtualMachineSpec struct {
+	MissionRef  string       `json:"missionRef,omitempty"`
+	ForProvider ProviderData `json:"forProvider,inline"`
+}
+
 type VirtualMachineStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
