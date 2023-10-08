9fb3d729b7f66c18d363a548c6be43c39f411037
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-10-08 11:07:19 -0600

    Failing to create test

--- internal/controller/mission/mission_controller_test.go
+++ internal/controller/mission/mission_controller_test.go
@@ -17,7 +17,30 @@ limitations under the License.
 package missioncontroller
 
 import (
+	"path/filepath"
+	"testing"
+
+	missionv1alpha1 "github.com/holy-tech/Mission-Control-Operator/api/mission/v1alpha1"
+	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/envtest"
 )
 
 var c client.Client
+
+func TestConfirmCRD(t *testing.T) {
+	testEnv = &envtest.Environment{
+		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
+		ErrorIfCRDPathMissing: true,
+	}
+	cfg, err := testEnv.Start()
+	Expect(err).NotTo(HaveOccurred())
+	Expect(cfg).NotTo(BeNil())
+
+	err = missionv1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	Expect(err).NotTo(HaveOccurred())
+	Expect(k8sClient).NotTo(BeNil())
+}
