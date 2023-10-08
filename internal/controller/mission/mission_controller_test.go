770f15234b46b3404d9bf2ef6a900f29a8b05aa5
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-10-08 16:24:27 -0600

    Pass test for applying gcp mission

--- internal/controller/mission/mission_controller_test.go
+++ internal/controller/mission/mission_controller_test.go
@@ -21,6 +21,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	missionv1alpha1 "github.com/holy-tech/Mission-Control-Operator/api/mission/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -28,12 +29,28 @@ import (
 
 var c client.Client
 
-var _ = Describe("Mission controller", func() {
-	Context("Creating Mission", func() {
-		It("Should apply mission definition", func() {
-			By("Creating new mission")
+var _ = Describe("GCP Mission controller", func() {
+	Context("Creating GCP Mission", func() {
+		It("Should apply gcp mission definition", func() {
+			By("Creating new gcp mission")
 			ctx := context.Background()
-			mission := &missionv1alpha1.Mission{}
+			mission := &missionv1alpha1.Mission{
+				// TypeMeta: metav1.TypeMeta{},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "mission-sample-gcp",
+				},
+				Spec: missionv1alpha1.MissionSpec{
+					Packages: []missionv1alpha1.PackageConfig{{
+						Provider:  "gcp",
+						ProjectID: "made-up-project-id",
+						Credentials: missionv1alpha1.CredentialConfig{
+							Name:      "missionkey-sample-gcp",
+							Namespace: "default",
+							Key:       "creds",
+						},
+					}},
+				},
+			}
 			Expect(k8sClient.Create(ctx, mission)).Should(Succeed())
 		})
 	})
