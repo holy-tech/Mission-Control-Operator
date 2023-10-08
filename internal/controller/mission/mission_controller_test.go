31d53d5b19061c2a9e9dbf932c6b9759f745b07b
Héctor de la Rosa Prado <hoftherose@gmail.com>
2023-10-08 15:58:06 -0600

    Customize ginko testing example

--- internal/controller/mission/mission_controller_test.go
+++ internal/controller/mission/mission_controller_test.go
@@ -17,30 +17,24 @@ limitations under the License.
 package missioncontroller
 
 import (
-	"path/filepath"
-	"testing"
+	"context"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 
 	missionv1alpha1 "github.com/holy-tech/Mission-Control-Operator/api/mission/v1alpha1"
-	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
 )
 
 var c client.Client
 
-func TestConfirmCRD(t *testing.T) {
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:     []string{filepath.Join("..", "..", "config", "crd", "bases")},
-		ErrorIfCRDPathMissing: true,
-	}
-	cfg, err := testEnv.Start()
-	Expect(err).NotTo(HaveOccurred())
-	Expect(cfg).NotTo(BeNil())
-
-	err = missionv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).NotTo(HaveOccurred())
-	Expect(k8sClient).NotTo(BeNil())
-}
+var _ = Describe("Mission controller", func() {
+	Context("Creating Mission", func() {
+		It("Should apply mission definition", func() {
+			By("Creating new mission")
+			ctx := context.Background()
+			mission := &missionv1alpha1.Mission{}
+			Expect(k8sClient.Create(ctx, mission)).Should(Succeed())
+		})
+	})
+})
